e1f2e31f4e4ad5f6d056d03e988063e9f6e46ccd
q050cr <reich.c@hotmail.com>
2022-11-12 21:30:46 +0100

    updated main script + render

--- scripts/render_param_reports.R
+++ scripts/render_param_reports.R
@@ -1,41 +1,52 @@
 
 
-## render report and mail results
-library(gmailr)
-library(dplyr)
+
 
 rm(list=ls())  # not in rmd doc, otherwise params deleted
 
-# define render function to stratify groups
+# change libpath
+myPaths <- .libPaths()
+myPaths <- c("library", myPaths)[1:2]  # only use ow
+.libPaths(myPaths)
+
+## render report and mail results
+library(dplyr)
+
 render_report = function(disease, analysis) {
   ## disease: acs, cad, hfref, dcm | default: dcm
   ## analysis: selected, full | default: selected
   
   rmarkdown::render(
-    "bestageing2022/scripts/20221110main.Rmd", 
+    "bestageing2022/scripts/20221110main.Rmd",
     params = list(
       disease = disease,
       analysis = analysis,
-      doc_title = paste0("Diagnostic performance of miRNAs in ", stringr::str_to_upper(disease) )
+      doc_title =  paste0("Diagnostic performance of miRNAs in ", stringr::str_to_upper(disease) )
     ),
-    output_file <<- paste0(
-      format(Sys.time(), "%Y%m%d"), '_ba_diagnostic_DISEASE_', stringr::str_to_upper(disease),
-      "_ANALYSIS_", stringr::str_to_upper(analysis), '.html'
-      ),
+    output_file = paste0(
+      format(Sys.time(), "%Y%m%d"), '_ba_diagnostic_DISEASE_', stringr::str_to_upper(disease), 
+      "_ANALYSIS_", stringr::str_to_upper(analysis),
+      '.html'
+    ),
     output_dir = "bestageing2022/reports",
     envir = globalenv()
   )
+  # save filename for sending later
+  filename.html <<-  paste0("bestageing2022/reports/",
+                            format(Sys.time(), "%Y%m%d"), '_ba_diagnostic_DISEASE_', stringr::str_to_upper(disease),
+                            "_ANALYSIS_", stringr::str_to_upper(analysis), '.html'
+  )
+  title.mail <<- paste0("Diagnostic performance of miRNAs in ", stringr::str_to_upper(disease) )
 }
 
-render_report(disease = "dcm", analysis = "selected")
-
-
+# render_report(disease = "dcm", analysis = "selected")
+# render scripts ----------------------------------------------------------
 
 diseases <- c("dcm", "acs", "cad", "hfref")
 analysis <- c("selected", "full")
 all_combis <- tidyr::crossing(diseases, analysis) %>% 
-  filter(analysis=="selected") %>% 
-  slice(1:2)
+  filter(analysis=="selected" & diseases=="dcm")# %>% 
+  #slice(1:2)
 
 ## render first report
 #time1 <- Sys.time()
@@ -49,13 +60,14 @@ for (i in 1:nrow(all_combis)) {
   render_report(disease = all_combis$diseases[i], analysis = all_combis$analysis[i])
   # send report
   email <-
-   gm_mime() %>%
-   gm_to("reich.c@hotmail.com") %>%
-   gm_from("q050cr@gmail.com") %>%
-   gm_subject(paste0("Job finished: ", output_file)) %>%
-   gm_text_body("Hi Christoph,\nHere is a new report for you.\nCheers")
-  email <- gm_attach_file(test_email, paste0("bestageing2022/reports/", output_file))
-  gm_send_message(email)
+   gmailr::gm_mime() %>%
+   gmailr::gm_to("reich.c@hotmail.com") %>%
+   gmailr::gm_from("q050cr@gmail.com") %>%
+   gmailr::gm_subject(paste0("Job finished: ", title.mail)) %>%
+   gmailr::gm_text_body("Hi Christoph,\nHere is a new report for you.\nCheers")
+  # attachment
+  email <- gmailr::gm_attach_file(email, filename.html)
+  gmailr::gm_send_message(email, user_id="me")
 }
 time22 <- Sys.time()
 time.diff.report <- time22-time11
