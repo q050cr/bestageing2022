7d4fb52e6fe71c93f9dcd3ff7a2ed212a212c253
c1node21 <reich.c@hotmail.com>
2023-06-14 12:42:35 +0000

    changed dir structure, harmonized miRetrieve literature miRNAs to version v21

---
+++ scripts/miRetrieve/harmonise-miRetrieve-v21.R
@@ -0,0 +1,84 @@
+
+
+version$version.string
+
+#  Load libraries
+library(miRetrieve); packageVersion('miRetrieve')
+library(magrittr) # Load magrittr for %>%
+library(ggplot2) # Load ggplot2 for plotting
+library(glue)
+library(rcrossref)
+library(dplyr) # Data wrangling
+library(tidyr) # Data wrangling
+library(miRBaseConverter)
+library(janitor)
+saveFILE <- TRUE
+
+##
+# Harmonize literature miRNAs to v21 (best version in bestageing mirna naming) -----------------------------
+##
+diseases <- c("ACS", "CAD", "DCM", "HFrEF")
+subset <- "-human" # changed to "-human" only, could also be changed to "-humantrials" only, or "" for full pubmed search !!!
+
+load("/mnt/users/reich/BestAgeing/data/data.rda")
+# get some miRNAs from data's colnames
+miRNANames <- colnames(data[10:2558])
+length(miRNANames)
+version = miRBaseConverter::checkMiRNAVersion(miRNANames, verbose = TRUE) 
+result1 = miRBaseConverter::miRNA_NameToAccession(miRNANames,version = c("v21"))  # get Accession MIMAT (dentifier that define miRNA uniquely in miRBase)
+result1[1:10,]
+
+# Use the vectorized glue function to generate a vector of strings
+path2files <- glue::glue_data(.x = data.frame(diseases = diseases),
+                             "./data-literature/miRetrieve/{diseases}/2023-06-13", {subset}, "-df_count_both.rds")
+path2files.bm <- glue::glue_data(.x = data.frame(diseases = diseases),
+                                 "./data-literature/miRetrieve/{diseases}/2023-06-13", {subset}, "-disease_biomarker.rds")
+
+for (data in seq_along(path2files)) {
+  df_count_both <- readRDS(path2files[data])
+  disease_biomarker <- readRDS(path2files.bm[data])  # also has biomarker_score
+  
+  ## fast conversion ----------------------------------------------------------
+  convert.fast = miRBaseConverter::miRNAVersionConvert(paste0("hsa-", df_count_both$miRNA),targetVersion = "v21",exact = TRUE, verbose = TRUE) # default exact and verbose are TRUE
+  convert.fast.bm = miRBaseConverter::miRNAVersionConvert(paste0("hsa-", unique(disease_biomarker$miRNA)),targetVersion = "v21",exact = TRUE, verbose = TRUE)  # also better than manual conversion  
+  
+  convert.fast <- convert.fast %>%
+    separate_rows(TargetName, Accession, sep = "&")
+  convert.fast.bm <- convert.fast.bm %>%
+    separate_rows(TargetName, Accession, sep = "&")
+  
+  df_count_both_accession <- df_count_both %>% 
+    mutate(miRNA = paste0("hsa-", miRNA)) %>% 
+    left_join(convert.fast, by = c("miRNA"="OriginalName"))
+  
+  disease_biomarker_accession <- disease_biomarker %>% 
+    mutate(miRNA = paste0("hsa-", miRNA)) %>% 
+    left_join(convert.fast.bm, by = c("miRNA"="OriginalName"), relationship = "many-to-many")
+  
+  # SAVE
+  if (saveFILE == TRUE) {
+    # df_count_both_accession
+    saveRDS(object = df_count_both_accession, 
+            file = glue::glue("./data-literature/miRetrieve/{diseases[disease]}/{Sys.Date()}{subset}-df_count_both_with_accession.rds"))
+    write.csv2(x = df_count_both_accession, file = glue::glue("./data-literature/miRetrieve/{diseases[disease]}/{Sys.Date()}{subset}-df_count_both_with_accession.csv"))
+    # disease_biomarker_with_accession
+    saveRDS(object = disease_biomarker_accession, 
+            file = glue::glue("./data-literature/miRetrieve/{diseases[disease]}/{Sys.Date()}{subset}-disease_biomarker_with_accession.rds"))
+    write.csv2(x = disease_biomarker_accession, file = glue::glue("./data-literature/miRetrieve/{diseases[disease]}/{Sys.Date()}{subset}-disease_biomarker_with_accession.csv"))
+  }
+  
+  ## manually match to most frequent version -----------------------------------
+  version = miRBaseConverter::checkMiRNAVersion(paste0("hsa-", df_count_both$miRNA), verbose = TRUE) 
+  result1 = miRBaseConverter::miRNA_NameToAccession(paste0("hsa-", df_count_both$miRNA),version = version) 
+  # match to second most frequent version
+  index <- which(is.na(result1[2]))
+  version2 = miRBaseConverter::checkMiRNAVersion(paste0("hsa-", df_count_both$miRNA[index]), verbose = TRUE) 
+  result2 = miRBaseConverter::miRNA_NameToAccession(paste0("hsa-", df_count_both$miRNA[index]),version = version2)
+  # combine to result1
+  result1$Accession[index] <- result2$Accession
+  
+  # miRBase Accessions to miRNA Names of the target version
+  convert.manual = miRBaseConverter::miRNA_AccessionToName(result1[,2],targetVersion = "v21")
+}
+
+
